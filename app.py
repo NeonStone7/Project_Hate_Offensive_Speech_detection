89dd33051b8edd79dc7226e87ea44d1d1547ef42
Oamen_Modupe <84418297+NeonStone7@users.noreply.github.com>
2023-12-30 15:33:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,11 +67,11 @@ def main():
     st.title("Hate speech and Offensive speech Detection")
 
     # description
-    st.markdown("""This model was trained on 24783 tweets to detect whether a model contains hate or offensive speech.                     
-                Train Score: 99%
-                Test auc: 70%
-                Validation auc: 77%.                          
-                Note: Model is overfitting and may not generalize well ☹.
+    st.markdown("""This model was trained on 24783 tweets to detect whether a model contains hate or offensive speech.\n        
+                Train Score: 99%\n
+                Test auc: 70%\n
+                Validation auc: 77%.\n                          
+                Note: Model is overfitting and may not generalize well ☹.\n
                 To do: Model generalization will be improved""")
 
     
